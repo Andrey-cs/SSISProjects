d622e70da795f33a25d476d09cef04114d1f8681
Andrey-cs <andreygwynn.monsanto@g.msuiit.edu.ph>
2025-04-30 23:59:42 +0800

    Add files via upload

---
+++ SSIS V1/sections/database/database.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import os
+
+class database:
+    def __init__(self, filename: str, columns: list):
+        self.columns = columns
+        self.filename = f"./sections/database/{filename}.csv"  
+        self.initialize()
+
+    def initialize(self):
+        """Create CSV file with headers if it doesn't exist."""
+        os.makedirs(os.path.dirname(self.filename), exist_ok=True)  
+        if not os.path.exists(self.filename):
+            df = pd.DataFrame(columns=self.columns)
+            df.to_csv(self.filename, index=False)
+
+    def get_all(self):
+        """Return all records as a list of dictionaries."""
+        df = pd.read_csv(self.filename)
+        return df.to_dict('records')
+
+    def check(self, id_to_check):
+        """Check if a record with the given ID exists (case-insensitive)."""
+        df = pd.read_csv(self.filename)
+        return (df[self.columns[0]].str.upper() == id_to_check.upper()).any()  
+
+    def insert_one(self, data):
+        for key in list(data.keys()):
+            data[key] = data[key].upper()
+        df = pd.read_csv(self.filename)
+        new_df = pd.DataFrame([data])
+        df = pd.concat([df, new_df], ignore_index=True)
+        df.to_csv(self.filename, index=False)
+        return True
+
+    def edit(self, data):
+        for key in list(data.keys()):
+            data[key] = data[key].upper()
+        df = pd.read_csv(self.filename)
+        key_column = self.columns[0]  
+        for index, row in df.iterrows():
+            if row[key_column] == data[key_column]:
+                for key in data.keys():
+                    df.at[index, key] = data[key]
+                break
+        df.to_csv(self.filename, index=False)
+        return True
+
+    def remove(self, id_to_remove):
+        df = pd.read_csv(self.filename)
+        key_column = self.columns[0]  
+        df = df[df[key_column] != id_to_remove]
+        df.to_csv(self.filename, index=False)
+
+# Initialize databases with YOUR column names
+programs = database('programsInfo', ["Program Code", "Program Name"])
+colleges = database('collegesInfo', ["College Code", "College Name"])
+students = database('studentsInfo', ["ID", "First Name", "Last Name", "Sex", "Age", "Program Code", "Year Level"])
